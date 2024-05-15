2ef77450e2836c5036fb7c9031e348c46a8de8b2
bradleyward <wardb2@cardiff.ac.uk>
2024-05-15 22:10:06 +0100

    SGP DMFs notebook

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,7 @@ from utils.rcparams import rcparams
 from utils.data_utils import full_loader, load_result, save_result
 from utils.astro_utils import dH, E, dC, dM, dA, dL, differential_comoving_v, blackbody, flux_to_luminosity, luminosity_to_flux, luminosity_to_dust_mass, dust_mass_to_luminosity, flux_to_dust_mass, dust_mass_to_flux, convert_observed_fluxes
 from utils.completeness import id_completeness
-from utils.literature import dust_mass_density, dust_mass_density_literature
+from utils.literature import dust_mass_density, dust_mass_density_literature, convert_literature
 from utils.galaxy import one_temperature, fit_one_temperature, Galaxy, create_sample
 from utils.binned_function import v, Survey
 from utils.monte_carlo import log_schechter_perdex, mcmc, mcmc_array
